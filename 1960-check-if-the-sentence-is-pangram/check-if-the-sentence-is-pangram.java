cac0db6f781ddc08c4d652b4e415040430efa2c7
Meet946 <meetmodi094@gmail.com>
2026-01-08 20:25:25 +0530

    Time: 3 ms (40.94%) | Memory: 43.1 MB (20.24%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        
+        Set<Character> set = new HashSet<>();
+        
+        for (char c : sentence.toCharArray()) {
+            set.add(c);
+        }
+        
+        
+        return set.size() == 26;
+    }
+}
