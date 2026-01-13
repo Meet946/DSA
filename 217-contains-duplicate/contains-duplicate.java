606deeaabd28d5ea279c3137f11c60f949fcbc0f
Meet946 <meetmodi094@gmail.com>
2026-01-13 13:16:34 +0530

    Time: 16 ms (45.72%) | Memory: 93.6 MB (42.58%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,17 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-         HashMap<Integer, Integer> map = new HashMap<>();
-        // count frequency
-        for(int num : nums){
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        //Find majority element
-        int n = nums.length;
-        for (int key : map.keySet()) {
-            if (map.get(key) > 1) {
-                return true;
-            }
-        }
-        return false;
+       Set<Integer>set=new HashSet<>();
+      for(int i:nums){
+        if(set.contains(i)) return true;
+        set.add(i);
+      }
+     return false;
     }
 }
