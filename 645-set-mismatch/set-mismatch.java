8ca4390b115404187465c78ae59b5337e40e6336
Meet946 <meetmodi094@gmail.com>
2026-01-18 13:48:28 +0530

    Time: 3 ms (62.74%) | Memory: 47.6 MB (56.15%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -2,6 +2,7 @@ class Solution {
     public int[] findErrorNums(int[] nums) {
         int[] ans = new int[2];
         int i = 0;
+        // cyclic sort
         while(i < nums.length){
             int correct = nums[i] - 1;
             if(nums[i] != nums[correct]){
@@ -11,6 +12,7 @@ class Solution {
                 i++;
             }      
         } 
+        // to find mismatch pair
         for(int j = 0; j < nums.length; j++){
             if(nums[j] != j + 1){
                 ans[0] = nums[j];
