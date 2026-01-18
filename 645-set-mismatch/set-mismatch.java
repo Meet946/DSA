62bf7ba983611344e89f6c1bfe2a53329482cef8
Meet946 <meetmodi094@gmail.com>
2026-01-18 13:47:24 +0530

    Time: 3 ms (62.74%) | Memory: 47.6 MB (56.15%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
+        int[] ans = new int[2];
         int i = 0;
         while(i < nums.length){
             int correct = nums[i] - 1;
@@ -12,10 +13,12 @@ class Solution {
         } 
         for(int j = 0; j < nums.length; j++){
             if(nums[j] != j + 1){
-                return new int[]{nums[j], j + 1};
+                ans[0] = nums[j];
+                ans[1] = j + 1;
+                break;
             }
         }
-        return new int[]{-1,-1};
+        return ans;
     }
 
     static void swap(int[] arr, int first, int second) {
