4be51b187a521e5adb9c916f94045cf8f616dd6e
Meet946 <meetmodi094@gmail.com>
2026-01-19 08:33:36 +0530

    Time: 11 ms (97.71%) | Memory: 47.4 MB (92.93%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.java
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.java
@@ -3,16 +3,14 @@ class Solution {
         // sort the array
         Arrays.sort(nums);
 
-        int ans = 0;
         // base condition : If the side of a triangle are a,b,c then a+b>c for the triangle to be formed .
         // find ans according to condition
         for(int i = nums.length -1 ; i > 1; i--){
             if(nums[i] < nums[i-1] + nums[i-2]){
-                ans = nums[i] + nums[i-1] + nums[i-2];
-                break;
+                return nums[i] + nums[i-1] + nums[i-2];
             }
         }
         
-        return ans;
+        return 0;
     }
 }
