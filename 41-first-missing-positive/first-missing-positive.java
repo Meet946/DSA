78845508fc408fea6be96f162d9443ca47b47ed6
Meet946 <meetmodi094@gmail.com>
2026-01-19 07:54:44 +0530

    Time: 1 ms (99.59%) | Memory: 70.9 MB (96.39%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        // cyclic sort by ignoring negative values
+        while(i < nums.length){
+            int correct = nums[i] - 1;
+            if(nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[correct]){
+                swap(nums, i, correct);
+            }
+            else{
+                i++;
+            }
+        }
+        // check for smallest positive missing integer
+        for(int j = 0; j < nums.length; j++){
+            if(nums[j] != j + 1){
+                return j+1;
+            }
+        }
+        return nums.length + 1;
+    }
+    static void swap(int[] nums, int first, int second){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
