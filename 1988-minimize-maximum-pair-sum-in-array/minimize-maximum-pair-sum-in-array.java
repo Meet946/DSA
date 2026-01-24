cc16b3cb84c7d299e99b21d0555086c671fe4294
Meet946 <meetmodi094@gmail.com>
2026-01-24 22:42:33 +0530

    Time: 56 ms (96.34%) | Memory: 97 MB (61.03%) - LeetSync

--- 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.java
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.java
@@ -3,17 +3,14 @@ class Solution {
         // first sort the array
         Arrays.sort(nums);
 
-        int pair_sum = 0, max_sum = 0 ;
+        int max_sum = 0 ;
         int left= 0, right = nums.length - 1;
 
-        // generate pair_sum and compute max_sum by using sorted array and two pointers
+        // generate max_sum by using sorted array and two pointers
         while(left < right){
-            pair_sum = nums[left] + nums[right];
+            max_sum = Math.max( max_sum, nums[left] + nums[right]);
             left++;
             right--;
-            if(max_sum < pair_sum){
-                max_sum = pair_sum;
-            }
         }
         return max_sum;
     }
