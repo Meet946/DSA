60a53e350d6ff76c7ae18da7280448a60edeb436
Meet946 <meetmodi094@gmail.com>
2026-01-25 13:10:41 +0530

    Time: 17 ms (5.36%) | Memory: 51.5 MB (46.88%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        // sort childrens greed factor
+        Arrays.sort(g);
+
+        // sort cookie sizes
+        Arrays.sort(s);
+
+        int i = 0; // for greed factor
+        int j = 0; // for cookie size
+        int count = 0;
+
+        while( i < g.length && j < s.length){
+            if( s[j] >= g[i]){
+                i++;
+                j++;
+                count++;
+            }
+            else{
+                j++;
+            }
+        }
+
+        return count;
+
+    }
+}
