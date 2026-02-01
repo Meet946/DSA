9b507c30adaa6e5bf2c012db6156bd899df9e78f
Meet946 <meetmodi094@gmail.com>
2026-02-01 18:08:32 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (86.63%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,14 +1,19 @@
 class Solution {
     public void sortColors(int[] nums) {
-        for(int i = 0; i < nums.length; i++){
-            //for each step , max item will come at the last respective index
-            for(int j = 1; j < nums.length - i; j++){
-                if(nums[j] <= nums[j - 1]){
-                    // swap
-                    int temp = nums[j];
-                    nums[j] = nums[j - 1];
-                    nums[j - 1] = temp;
-                }
+        // as max element is 2
+        int[] count = new int[3];
+
+        // store the frequency of elements 
+        for(int num : nums){
+            count[num]++;
+        }
+
+        // re-build the array
+        int index = 0;
+        for(int i = 0; i < count.length; i++){
+            while(count[i] > 0){
+                nums[index++] = i;
+                count[i]--;
             }
         }
     }
