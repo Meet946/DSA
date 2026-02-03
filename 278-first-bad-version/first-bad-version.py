0e6a0f037a7d9109e12c6c782c3956a99d38aedb
Meet946 <meetmodi094@gmail.com>
2026-02-03 22:01:11 +0530

    Time: 12 ms (73.83%) | Memory: 12.4 MB (56.01%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -9,15 +9,13 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        start, end = 1, n
-
-        while start <= end :
-            mid = (start + end)// 2
-
-            if isBadVersion(mid) :
-                end = mid - 1
-
-            else: 
-                start = mid + 1
-
-        return start
+        low=0
+        high=n
+        mid=0
+        while low<high:
+            mid=(low+high)//2
+            if isBadVersion(mid):
+                high=mid
+            else:
+                low=mid+1
+        return low
