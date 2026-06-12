af561c40ac6755f987d15c27784fe3f539b1e464
Meet946 <meetmodi094@gmail.com>
2026-06-12 11:28:44 +0530

    Time: 17 ms (17.22%) | Memory: 53.4 MB (7.31%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,20 +1,27 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int targetFrequency = nums.length / 3;
+        int targetFrequency = nums.length / 3 + 1;
 
         Map<Integer, Integer> frequencyMap = new HashMap<>();
 
         List<Integer> result = new ArrayList<>();
 
-        for(int num: nums){
-            frequencyMap.put(num, frequencyMap.getOrDefault(num,0) + 1);
+        for(int i = 0; i < nums.length; i++){
+
+            frequencyMap.put(nums[i], frequencyMap.getOrDefault(nums[i],0) + 1);
+
+            if(frequencyMap.get(nums[i]) == targetFrequency){
+                result.add(nums[i]);
+            }
+
+            if(result.size() == 2) break;
         }
 
-        for(Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-            if(entry.getValue() > targetFrequency){
-                result.add(entry.getKey());
-            }
-        }
+        // for(Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+        //     if(entry.getValue() > targetFrequency){
+        //         result.add(entry.getKey());
+        //     }
+        // }
 
         return result;
     }
