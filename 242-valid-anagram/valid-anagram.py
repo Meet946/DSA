1be7d879168d2847e4b08f993db94a179c2e61f8
Meet946 <meetmodi094@gmail.com>
2026-07-27 19:44:44 +0530

    Time: 15 ms (44.34%) | Memory: 19.4 MB (45.81%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        freq = {}
+        for char in s:
+            freq[char] = freq.get(char, 0) + 1
+
+        for char in t:
+            if char not in freq.keys():
+                return False
+            freq[char] -= 1
+        
+        for value in freq.values():
+            if value != 0:
+                return False
+
+        return True
+            
+        
+        
