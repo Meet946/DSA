c1ee0335a797bf524465d09027752cc8aa347b34
Meet946 <meetmodi094@gmail.com>
2026-08-03 15:54:40 +0530

    Time: 1352 ms (9.63%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,8 @@
+
+SELECT r.contest_id, 
+    ROUND((COUNT(user_id) / (SELECT COUNT(*) FROM Users)) * 100,
+     2) AS percentage
+FROM Register AS r
+GROUP BY r.contest_id
+ORDER BY percentage DESC,
+    r.contest_id ASC;
